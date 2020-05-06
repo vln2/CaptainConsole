debbe6571a5e124c167fc17739cb4de6ff6cb626
MyraMidnight <dark_stardragon@hotmail.com>
2020-05-06 21:49:19 +0000

    created app for pages and a home_view as a test, displayes pages in browser

--- CaptainConsole/urls.py
+++ CaptainConsole/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import list_products
+
+# the pages
+from pages.views import home_view
+from pages.views import list_products
 
 urlpatterns = [
+    path('', home_view, name="home_view"),  # renders page at root of website
     path('admin/', admin.site.urls),
-    path('list', list_products, name='list_products')
+    path('list', list_products, name='list_products'),
 ]
