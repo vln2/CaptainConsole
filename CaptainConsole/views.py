c2cf8550d429e20712609771fdf0d74d48e3e735
MyraMidnight <dark_stardragon@hotmail.com>
2020-05-07 16:44:44 +0000

    header is now displaying from the static folder

--- CaptainConsole/views.py
+++ CaptainConsole/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
-
-#views created
 from .models import Product
 
-
+#======================= REGISTER USER
 def register_user(request):  
     form = UserCreationForm()
 
@@ -16,22 +14,23 @@ def register_user(request):
 
     return render(request, 'register.html')
 
-
+#======================= LOGIN
 def user_login(request):
     context = {}
     return render(request, 'login.html', context)
 
-
+#======================= CATEGORY / LIST_PRODUCTS
 def list_products(request):
     context = {
         'products': Product.objects.all()
     }
     return render(request, 'catalog/list_products.html', context)
 
-
+#======================= SINGLE PRODUCT
 def single_product(response, id):
 
     context = {
         'product': Product.objects.get(id=id)
     }
+    print(context['product'])
     return render(response, 'product/single_product.html', context)
