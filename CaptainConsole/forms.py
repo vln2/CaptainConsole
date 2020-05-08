a8784a2da3a9130f8b165b5bc0a146a2eac3aada
MyraMidnight <dark_stardragon@hotmail.com>
2020-05-08 16:33:37 +0000

    working a bit on styling the form

--- CaptainConsole/forms.py
+++ CaptainConsole/forms.py
@@ -14,4 +14,9 @@ class CreatingUserForms(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
