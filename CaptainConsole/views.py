025bc9d64d1ca684441f4e130e6d175d60728626
Steinar Snær Guðjónsson <steinar@fjarvirkni.is>
2020-05-11 20:34:33 +0000

    úps þetta hér gleymdist átti að vera í commit 820c3bb0dfc5a8dcf2a3926c3753fee1bd1f7645

--- CaptainConsole/views.py
+++ CaptainConsole/views.py
@@ -1,12 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView, DetailView
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import Product, Category, ProductImage
+from .models import Product, Category, ProductImage, Order, Item
 from django.db.models import F
-from .forms import CreatingUserForms
+from .forms import CreatingUserForms, AddItemToCartForm
 
 
 def registerUser(request):
@@ -71,12 +72,46 @@ def show_category(request, hierarchy=None):
     categories_slug = hierarchy.split('/')
     category_slug = categories_slug[-1]
     category = Category.objects.get(slug=category_slug)
+    products = category.get_products()
+    paginator = Paginator(products, 20)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
+        'add_to_cart_form': AddItemToCartForm(),
         'instance': category,
-        'products': category.get_products,
+        'products': page_obj,
         'breadcrumbs': category.get_ancestors(include_self=True)
     }
+
     return render(request, 'pages/product_list.html', context)
 
-#passa að bæta við @login_required(login_url='login') ef notandi þarf að vera skráður inn til að gera einhvað ákveðið
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    form = AddItemToCartForm(request.POST or None)
+    if form.is_valid():
+
+        order_qs = Order.objects.filter(owner=request.user, status=Order.CART)
+        if order_qs.exists():
+            order = order_qs[0]
+        else:
+            order = Order.objects.create(owner=request.user)
+
+        item_qs = order.items.filter(product__id=product.id)
+        if item_qs.exists():
+            item = item_qs[0]
+            item.quantity += 1
+            item.save()
+            messages.info(request, "This item quantity was updated.")
+            # return redirect("core:order-summary")
+        else:
+            item = Item.objects.create(product=product)
+            order.items.add(item)
+            messages.info(request, "This item was added to your cart.")
+            # return redirect("core:order-summary")
+
+    httpheaders = request.headers
+    refer = httpheaders['Referer']
+    return redirect(refer)
