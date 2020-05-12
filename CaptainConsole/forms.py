203e130a5fd23609b137775777578f1599755bbc
maysileefox <thorgerdura@gmail.com>
2020-05-12 01:38:03 +0000

    Fixed login issue its working now

--- CaptainConsole/forms.py
+++ CaptainConsole/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -23,6 +23,11 @@ class CreatingUserForms(UserCreationForm):
             'password2': forms.PasswordInput(attrs={'class': 'form-control col-sm-10'})
         }
 
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username']
+        ...
 
 class AddItemToCartForm(ModelForm):
     class Meta:
