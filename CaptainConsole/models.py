f758ab119b3dc20a61ae5931262f323fbc5a2423
MyraMidnight <dark_stardragon@hotmail.com>
2020-05-14 10:35:59 +0000

    added discount field to Product in models, and valid_discount method

--- CaptainConsole/models.py
+++ CaptainConsole/models.py
@@ -53,9 +53,17 @@ class Product(models.Model):
     price = models.FloatField()
     slug = models.SlugField(null=False)
     thumb = models.ImageField(upload_to='images/thumbs/', default='No_image_available.png', blank=True)
+    discount = models.IntegerField( default=0 ) 
 
     def __str__(self):
         return self.name
+
+    def valid_discound(self):
+        """Checks if the discount is valid (not higher than actual price)"""
+        if discount > price or discount == 0:
+            return False
+        else:
+            return True
 
 
 class ProductImage(models.Model):
