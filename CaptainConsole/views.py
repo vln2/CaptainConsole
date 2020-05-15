64d0460f51a840a8297a82f5e1d8370d2dae54d7
MyraMidnight <dark_stardragon@hotmail.com>
2020-05-15 22:45:37 +0000

    added cart_size to request.session to keep track of number of items in cart

--- CaptainConsole/views.py
+++ CaptainConsole/views.py
@@ -24,17 +24,6 @@ VALID_SORTS = {
 PRODUCTS_PER_PAGE = 12
 
 
-
-# ======================= SALES / FRONT PAGE
-
-def currentSales(request):
-    #get all products that are on sale
-    products = Product.objects.all() #Product.objects.filter('discount' > 0)
-    
-    context = {
-        'products':products
-    }
-    return render(request, 'pages/sales.html', context)
 # ======================= REGISTER USER
 def registerUser(request):
     #if request.user.is_authenticed:
@@ -133,6 +122,7 @@ def funcSearchHistory(request):
 
     return lRecentlyViewed
 
+#======================= PRODUCT LIST
 def productList(request, *args):
     products = Product.objects.all()
     #if the user wishes to sort by name/price
@@ -244,7 +234,14 @@ def addToCart(request, product_id):
         item[0].quantity += form.cleaned_data.get('quantity')
         item[0].save()
         messages.success(request, "{} was added to your cart.".format(str(product)))
-
+        
+        #session cookie keeps track of cart size
+        if 'cart_size' in request.session:
+            cart_size = request.session['cart_size'] + 1
+            request.session['cart_size'] = cart_size
+        else:
+            request.session['cart_size'] = 1
+            
     try:
         httpheaders = request.headers
         refer = httpheaders['Referer']
@@ -253,6 +250,7 @@ def addToCart(request, product_id):
     return redirect(refer)
 
 
+#======================= REMOVE FROM CART
 def removeFromCart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     userInfo = get_object_or_404(UserInfo, user=request.user)
@@ -276,6 +274,15 @@ def removeFromCart(request, product_id):
             else:
                 item.save()
             messages.info(request, "Item removed from cart")
+
+            #then update the session cookie
+            if 'cart_size' in request.session:
+                cart_size = request.session['cart_size']
+                if cart_size > 0:
+                    cart_size -= 1
+                    request.session['cart_size'] = cart_size
+            
+            
         else:
             messages.info(request, f"No item {product} was found in {request.user} cart")
 
